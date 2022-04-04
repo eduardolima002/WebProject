52845ee5169e89aa48602495aa194928ba8ae6f5
eduardolima002 <Eduardosalviano002@hotmail.com>
2022-04-04 18:21:59 -0300

    Other entites and second migration

--- WebApplication1/Models/Department.cs
+++ WebApplication1/Models/Department.cs
@@ -1,8 +1,28 @@
-﻿namespace WebApplication1.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        ICollection<Seller> Seller { get; set; } = new List<Seller>();   
+        
+
+        public Department() { 
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public double TotalSales(DateTime initian, DateTime final)
+        {
+            return Seller.Sum(seller => seller.TotalSales(initian, final));
+        }
     }
 }
---
+++ WebApplication1/Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public enum SaleStatus : int
+    {
+        PENDING = 0,
+        BILLED = 1,
+        CANCELED = 2
+    }
+}
