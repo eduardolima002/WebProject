6b7e45d1da3868572944b66362947f82064e4484
eduardolima002 <Eduardosalviano002@hotmail.com>
2022-04-05 18:06:32 -0300

    SellerService and FindAll

---
+++ WebApplication1/Services/SellerService.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using WebApplication1.Models;
+using System.Collections.Generic;
+using WebApplication1.Data;
+
+namespace WebApplication1.Services
+{
+    public class SellerService
+    {
+        private readonly WebApplication1Context _context;
+
+        public SellerService(WebApplication1Context context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
