1629c5fa9eb0928e1ad657151568f11497970895
eduardolima002 <Eduardosalviano002@hotmail.com>
2022-04-06 12:11:00 -0300

    Update seller and custom service exception

--- WebApplication1/Controllers/SellersController.cs
+++ WebApplication1/Controllers/SellersController.cs
@@ -2,7 +2,8 @@
 using WebApplication1.Services;
 using WebApplication1.Models;
 using WebApplication1.Models.ViewModels;
-
+using WebApplication1.Services.Exceptions;
+using System.Collections.Generic;
 
 
 namespace WebApplication1.Controllers
@@ -74,6 +75,45 @@ namespace WebApplication1.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewmodel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewmodel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- WebApplication1/Services/SellerService.cs
+++ WebApplication1/Services/SellerService.cs
@@ -3,6 +3,7 @@ using WebApplication1.Models;
 using System.Collections.Generic;
 using WebApplication1.Data;
 using Microsoft.EntityFrameworkCore;
+using WebApplication1.Services.Exceptions;
 
 namespace WebApplication1.Services
 {
@@ -37,6 +38,23 @@ namespace WebApplication1.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+        
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
 
 
