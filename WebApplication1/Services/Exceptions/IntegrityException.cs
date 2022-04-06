513542edfd9d787ed2c3122f455d8fd192551258
eduardolima002 <Eduardosalviano002@hotmail.com>
2022-04-06 17:46:45 -0300

    Exception handling for delete

---
+++ WebApplication1/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication1.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
